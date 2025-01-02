2a2f6c40693c9e115077347f671929aa0a68d300
notRyoros <marcel.iwastschenko@freenet.de>
2025-01-02 01:17:21 +0100

    Initial commit

---
+++ src/main/java/de/notgyros/cloud_mechanics/CloudMechanics.java
@@ -0,0 +1,16 @@
+package de.notgyros.cloud_mechanics;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CloudMechanics implements ModInitializer {
+	public static final String MOD_ID = "cloud_mechanics";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
